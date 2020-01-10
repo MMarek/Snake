9d1b8fbe19a927c70e77d54d2c36ab3c2a86dc29
MMarek <mildnermarek@gmail.com>
2020-01-10 12:06:39 +0100

    corrects of global changes in code

--- web.js
+++ web.js
@@ -1,55 +1,113 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+//
+// //mutable state
+// let state = initialState();
+//
+// //position helpers
+// const x = c => Math.round(c * canvas.width / state.cols);
+// const y = r => Math.round(r * canvas.height / state.rows);
+//
+// //game loop draw
+// const draw = () => {
+//     //clear / czyszczenie
+//     ctx.fillStlye = '#232323';
+//     ctx.fillRect(0,0,canvas.width,canvas.height);
+//
+//     //draw snake / rysuj snake
+//     ctx.fillStlye = 'rgb(0,200,50)';
+//     state.snake.map(p => ctx.fillRect(x(p.x),y(p.y), x(1), y(1)));
+//
+//     //draw apple / rysuj jabłka
+//     ctx.fillStlye = 'rgb(255,50,0)';
+//     ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1));
+//
+//     //add crash / zderzenie
+//     if (state.snake.length == 0) {
+//         ctx.fillStlye = 'rgb(255,0,0)';
+//         ctx.fillRect(0,0,canvas.width,canvas.height)
+//     }
+// };
+//
+// //game loop update
+// const step = t1 => t2 => {
+//     if (t2 - t1 > 100) {
+//         state = next(state);
+//         draw();
+//         window.requestAnimationFrame(step(t2))
+//     } else {
+//         window.requestAnimationFrame(step(t1))
+//     }
+// };
+//
+// //key events
+// window.addEventListener('keydown', e=>{
+//     switch (e.key) {
+//         case 'w': case 'h': case 'ArrowUp': state = enqueue(state, NORTH); break;
+//         case 'a': case 'j': case 'ArrowLeft': state = enqueue(state, WEST); break;
+//         case 's': case 'k': case 'ArrowDown': state = enqueue(state, SOUTH); break;
+//         case 'd': case 'l': case 'ArrowRight': state = enqueue(state, EAST); break
+//     }
+// });
+//
+// //main
+// draw();
+// window.requestAnimationFrame(step(0));
 
-//mutable state
-let state = initialState();
 
-//position helpers
-const x = c => Math.round(c * canvas.width / state.cols);
-const y = r => Math.round(r * canvas.height / state.rows);
 
-//game loop draw
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+// Mutable state
+let state = initialState()
+
+// Position helpers
+const x = c => Math.round(c * canvas.width / state.cols)
+const y = r => Math.round(r * canvas.height / state.rows)
+
+// Game loop draw
 const draw = () => {
-    //clear / czyszczenie
-    ctx.fillStlye = '#232323';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    // clear
+    ctx.fillStyle = '#232323'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    //draw snake / rysuj snake
-    ctx.fillStlye = 'rgb(0,200,50)';
-    state.snake.map(p => ctx.fillRect(x(p.x),y(p.y), x(1), y(1)));
+    // draw snake
+    ctx.fillStyle = 'rgb(0,200,50)'
+    state.snake.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
 
-    //draw apple / rysuj jabłka
-    ctx.fillStlye = 'rgb(255,50,0)';
-    ctx.fillRect(x(state.apple.x),y(state.apple.y), x(1), y(1));
+    // draw apples
+    ctx.fillStyle = 'rgb(255,50,0)'
+    ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1))
 
-    //add crash / zderzenie
+    // add crash
     if (state.snake.length == 0) {
-        ctx.fillStlye = 'rgb(255,0,0)';
-        ctx.fillRect(0,0,canvas.width,canvas.height)
+        ctx.fillStyle = 'rgb(255,0,0)'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
     }
-};
+}
 
-//game loop update
+// Game loop update
 const step = t1 => t2 => {
     if (t2 - t1 > 100) {
-        state = next(state);
-        draw();
+        state = next(state)
+        draw()
         window.requestAnimationFrame(step(t2))
     } else {
-        window.requestAnimationFrame(step(t1));
+        window.requestAnimationFrame(step(t1))
     }
-};
+}
 
-//key events
-window.addEventListener('keydown', e=>{
+// Key events
+window.addEventListener('keydown', e => {
     switch (e.key) {
-        case 'w': case 'h': case 'ArrowUp': state = enqueue(state, NORTH); break;
-        case 'a': case 'j': case 'ArrowLeft': state = enqueue(state, WEST); break;
-        case 's': case 'k': case 'ArrowDown': state = enqueue(state, SOUTH); break;
-        case 'd': case 'l': case 'ArrowRight': state = enqueue(state, EAST); break
+        case 'w': case 'h': case 'ArrowUp':    state = enqueue(state, NORTH); break
+        case 'a': case 'j': case 'ArrowLeft':  state = enqueue(state, WEST);  break
+        case 's': case 'k': case 'ArrowDown':  state = enqueue(state, SOUTH); break
+        case 'd': case 'l': case 'ArrowRight': state = enqueue(state, EAST);  break
     }
-});
+})
 
-//main
-draw();
-window.requestAnimationFrame(step(0));
+// Main
+draw(); window.requestAnimationFrame(step(0))
